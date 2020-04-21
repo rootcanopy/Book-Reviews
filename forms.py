ceff387e8865cf404d12ad30bf956c4233d1e1ed
rootcanopy <dave.trees201@gmail.com>
2020-04-21 23:28:33 +0200

    login and register validators done in forms

--- forms.py
+++ forms.py
@@ -4,6 +4,17 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
-    
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()]
+    email = StringField('Email Address', validators=[DataRequired()], Email()])
+    password = PasswordField('Password',validators=[DataRequired(),EqualTo('password_repeat', message='Passwords must match')])                                   
+    password_repeat = PasswordField('Repeat Password')
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Register')
 
 
