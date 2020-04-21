3507f90a52c1328115354ab0c2fdeae765196d9b
rootcanopy <dave.trees201@gmail.com>
2020-04-21 21:01:58 +0200

    still trying to get reviews to render on page

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
 import os
+import env
 from flask import Flask, render_template, redirect, url_for, flash, request, session
-from flask_pymongo import PyMongo, DESCENDING
+from flask_pymongo import pymongo, PyMongo, DESCENDING
 from bson.objectid import ObjectId
 #from forms import RegistrationForm, LoginForm, ReviewForm
 from werkzeug.security import generate_password_hash, check_password_hash
-import env
 import math
 import re
+
 
 app = Flask(__name__)
 
 
 # SECRET KEY FOR CSRF #TODO + DB + MONGOURI
 app.config['MONGO_DBNAME'] = 'book_review'
-app.config['SECRET_KEY'] = '8bf1555c499fe3cc55021fd1e87585e5'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config['SECRET_KEY'] = '8bf1555c499fe3cc55021fd1e87585e5'
 
 
 mongo = PyMongo(app)
 
+# ROUTE FOR LAYOUT
+@app.route('/')
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
 
 # ROUTE FOR INDEX.HTML
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     # LANDING GETS 4 REVIEWS THAT HAVE THE MOST UPVOTES
-    reviews = mongo.db.reviews.find().sort([('upvote', DESCENDING)]).limit(4)
-    return render_template('index.html', title='Home', reviews=reviews)
+    four_reviews = mongo.db.reviews.find().sort([('upvote', pymongo.DESCENDING)]).limit(4)
+    
+    return render_template('index.html', title='Home', four_reviews=four_reviews)
+
+
+@app.route('/add_review')
+def add_review():
+
+    user = mongo.db.reviews
+    create_review = {'author': 'jack', 'title': 'My Toe', 'summary': 'how i stopped jamming'}
+    add_review = mongo.db.reviews.insert_one(create_review)
+    return 'review_added'
 
 
 
