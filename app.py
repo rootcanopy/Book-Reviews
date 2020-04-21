87489cae9c190b48d9c71bc0bc63c6765d23b749
rootcanopy <dave.trees201@gmail.com>
2020-04-21 17:35:58 +0200

    base setup and ready to create html and routes

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import env
+import math
+import re
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from forms import RegistrationForm, LoginForm, ReviewForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+app = Flask(__name__)
+
+
+# SECRET KEY FOR CSRF #TODO + DB + MONGOURI
+app.config['MONGO_DBNAME'] = 'book_review'
+app.config['SECRET_KEY'] = '8bf1555c499fe3cc55021fd1e87585e5'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://rootcanopy:Di1LJQyxmTBKIvCj@mycluster-t6yqh.mongodb.net/book_review?retryWrites=true&w=majority"
+
+os.environ["PORT"] = "5000"
