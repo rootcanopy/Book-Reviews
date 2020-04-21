8ef0c5a68525b853db5abb5eec521babc1070d68
rootcanopy <dave.trees201@gmail.com>
2020-04-21 18:28:20 +0200

    first route, index.html in and navbar in

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
-import env
-import math
-import re
 from flask import Flask, render_template, redirect, url_for, flash, request, session
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, DESCENDING
 from bson.objectid import ObjectId
 #from forms import RegistrationForm, LoginForm, ReviewForm
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import env
+import math
+import re
 
 app = Flask(__name__)
 
@@ -21,7 +20,12 @@ mongo = PyMongo(app)
 
 
 # ROUTE FOR INDEX.HTML
-
+@app.route('/')
+@app.route('/index')
+def index():
+    # LANDING GETS 4 REVIEWS THAT HAVE THE MOST UPVOTES
+    four_books = mongo.db.reviews.find().sort([('upvotes', DESCENDING)]).limit(4)
+    return render_template('index.html', title='Home', reviews=four_books)
 
 
 
