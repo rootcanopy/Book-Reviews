1e421bad1e77b24a5a9f44ba80bd47c88e70f54d
rootcanopy <dave.trees201@gmail.com>
2020-04-22 12:00:07 +0200

    cannot fingure out why index.html wont load

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import env
-from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask import Flask, render_template, redirect, \
+        url_for, flash, request, session
 from flask_pymongo import pymongo, PyMongo, DESCENDING
 from bson.objectid import ObjectId
 from forms import RegistrationForm, LoginForm, ReviewForm
@@ -24,108 +25,110 @@ mongo = PyMongo(app)
 @app.route('/index')
 def index():
     # LANDING GETS 4 REVIEWS THAT HAVE THE MOST UPVOTES
-    four_reviews = mongo.db.reviews.find().sort([('upvote', DESCENDING)]).limit(4)
+    four_reviews = mongo.db.reviews.find().sort('upvote',\
+                            pymongo.DESCENDING).limit(4)
     return render_template('index.html', title='Home', reviews=four_reviews)
 
 
-#ROUTE FOR REGISTER
+# ROUTE FOR REGISTER
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    #FUNCTION TO ALLOW A NEW USER TO REGISTER AND POST REVIEWS
+    # FUNCTION TO ALLOW A NEW USER TO REGISTER AND POST REVIEWS
     form = RegistrationForm(request.form)
     if form.validate_on_submit():
-        #IF USER PASSES AUTHENTICATION
+        # IF USER PASSES AUTHENTICATION
         user = mongo.db.users
         search_db = user.find_one({'username': request.form['username']})
-
+        # USER ISINT REGISTERED
         if search_db is None:
-            #IF SEARCH RETURNS NO USERS
+            # IF SEARCH RETURNS NO USERS
             pw_hash = generate_password_hash(request.form['password'])
-            #ENCRYPTS PASSWORD
+            # ENCRYPTS PASSWORD
             user.insert_one({'username': request.form['username'],
                             'password': pw_hash,
                             'email': request.form['email']})
-            #ALERT USER TO ACCOUNT CREATION
+            # ALERT USER TO ACCOUNT CREATION
             flash('You now registered and logged in', 'success')
             session['username'] = request.form['username']
             session['logged_in'] = True
             return redirect(url_for('my_account'))
         else:
-            #IF THAT DOESNT WORK
+            # IF THAT DOESNT WORK
             flash('Oops, something happened. Try again.', 'warning')
             return redirect(url_for('register'))
-
     return render_template('register.html', title='Register', form=form)
 
 
-#ROUTE FOR LOGIN
+# ROUTE FOR LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    #THE USER LOGIN FUNCTION
+    # THE USER LOGIN FUNCTION
     form = LoginForm()
     # LOGIN HANDLER
     if session.get('logged_in'):
         if session['logged_in'] is True:
             return redirect(url_for('home'))
-
     if form.validate_on_submit():
-
-        #IF LOG IN FORM PASSES VALIDATION
+        # IF LOG IN FORM PASSES VALIDATION
         users = mongo.db.users
         existing_user = users.find_one({'username': request.form['username']})
-        
-        if existing_user:  #IF USER IN DB
+        if existing_user:  # IF USER IN DB
             password = form.password.data
             if check_password_hash(existing_user['password'], password):
                 # IF THE HASH FITS
                 session['username'] = request.form['username']
                 session['logged_in'] = True
-                #IF SUCCESS REDIRECT TO HOME
+                # IF SUCCESS REDIRECT TO HOME
                 return redirect(url_for('index', title='Home', form=form))
-            #IF FAILS
+            # IF FAILS
             flash('Invalid username/password combination')
     return render_template('login.html', title='Log In', form=form)
 
 
+# ROUTE TO ACCOUNT
 @app.route('/my_account/')
 def my_account():
-    #FOR USER ACCOUNT
+    # FOR USER ACCOUNT
     if 'logged_in' in session:
+        # ASSIGNING CU ATTRIB
         current_user = session['username']
-        flash('Hi "' + current_user + '". This is your profile, add or edit your reviews', 'success')
-
+        # MESSAGE TO GREET USER
+        flash('Hi "' + current_user + '". This is your profile, add \
+                or edit your reviews', 'success')
+        # GETTING USER DETAILS FOR ACCOUNT
         search_user = mongo.db.users.find_one({'username': current_user})
-
+        # USERS REVIEW HISTORY
         reviews = mongo.db.reviews.find_one({'username': current_user})
-        loop = mongo.db.reviews.count_documents({'username': current_user})
+        loop = mongo.db.reviews.count_documents({'username': current_user})  # TODO
+    return render_template('my_account.html', title='My Account',
+                            loop=loop, users=search_user,
+                            reviews=reviews)
 
-    return render_template('my_account.html', title='My Account',  loop=loop, users=search_user, reviews=reviews)
 
-
-#TO REMOVE/DELETE ACCOUNT
+# TO REMOVE/DELETE ACCOUNT
 @app.route('/delete_account/<id>', methods=['GET', 'POST'])
 def delete_account(id):
-    #DETAILS FROM LOGGED IN
+    # DETAILS FROM LOGGED IN
     users = session['username']
-    #REMOVING ALL BY USER
+    # REMOVING ALL BY USER
     mongo.db.reviews.delete_many({'username': users})
-    #REMOVING ACCOUNT
+    # REMOVING ACCOUNT
     mongo.db.users.delete_one({'_id': ObjectId(id)})
-    #SESSION POPPED
+    # SESSION POPPED
     session.clear()
 
     flash('Your account has been removed', 'success')
     return redirect(url_for('index'))
 
 
-#ROUTE FORM ADD REVIEW
+# ROUTE FORM ADD REVIEW
 @app.route('/add_review', methods=['GET', 'POST'])
 def add_review():
     form = ReviewForm()
     if form.validate_on_submit():
         reviews = mongo.db.reviews
-        #ADD A RECORD
-        reviews.insert_one({ 
+        # ADD A RECORD
+        reviews.insert_one({
             'author': request.form['author'],
             'title': request.form['title'],
             'summary': request.form['summary'],
@@ -134,36 +137,35 @@ def add_review():
             'username': session['username']
         })
         flash('You have a reviewed a book', 'success')
-        #SENDS TO REVIEWS UPON SUCCESS
+        # SENDS TO REVIEWS UPON SUCCESS
         return redirect(url_for('my_reviews'))
-
     return render_template('add_review.html', title='Add Review', form=form)
 
 
-
-#THE LOGGED IN USERS REVIEWS
+# THE LOGGED IN USERS REVIEWS
 @app.route('/my_reviews', methods=['GET', 'POST'])
 def my_reviews():
     if 'logged_in' in session:
         flash('This is your reviews page', 'success')
-
+        # UPON ARRIVING ON USER REVIEW PAGE
         current_user = session['username']
-
-        review = mongo.db.reviews.find({'username': current_user})
-    
-    return render_template('my_reviews.html', title='My Reviews', review=review)
+        # SEARCH DB FOR USERS REVIEWS
+        reviews = mongo.db.reviews.find({'username': current_user})
+    return render_template('my_reviews.html', title='My Reviews',
+                reviews=reviews)
 
 
 # USER SIGN OUT
 @app.route('/logout')
 def logout():
-    #SIGNS USER OUT AND CLEARS SESSION
+    # SIGNS USER OUT AND CLEARS SESSION
     session.clear()
     flash('You have logged out', 'success')
     return render_template('index.html', title='Home')
 
-# APP RUN 
+
+# APP RUN
 if __name__ == '__main__':
-            app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+        app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
