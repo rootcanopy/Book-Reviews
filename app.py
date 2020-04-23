95e8b1cccd02e1424a1676be5bc58f00758c270b
rootcanopy <dave.trees201@gmail.com>
2020-04-23 11:44:32 +0200

    still fixing

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def logout():
 
 
 # APP RUN
-if __name__ == '__main__':
-        app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
         debug=False)
